b9c34b5f4e6ada447a021ab8427b49839cb92e2c
Ngocanh56 <email@example.com>
2023-11-24 00:44:56 +0700

    qldt

--- app/crud.py
+++ app/crud.py
@@ -1,7 +1,54 @@
 from sqlalchemy.orm import Session
 import models
+from datetime import datetime
+from sqlalchemy import func,asc
 
 
 def get_subject(db: Session, subject_id: int):
-    return db.query(models.SubjectClass).filter(models.Subject.id == subject_id).first()
-    
+    return db.query(models.SubjectClass).filter(models.SubjectClass.id == subject_id).all()
+
+
+def get_qldt(
+    db: Session, student_id: int = None, 
+    student_name: str = None, date_of_birth: datetime = None,
+    course_class_code: str = None, subject_class_id: int = None,
+    subject_code: str = None, subject_name: str = None, credit: int = None,
+    limit_number: int = None, semester_id: int = None):
+    query = (
+        db.query(models.Student.id, models.Student.name, models.Student.date_of_birth,
+                func.concat("K",models.CourseClass.gen, "-", models.Major.code).label('course_class_name'),
+                 models.Subject.subject_code, models.Subject.subject_name,
+                 models.Subject.credit)
+        .join(models.TakeClass, models.Student.id == models.TakeClass.student_id)
+        .join(models.SubjectClass, models.SubjectClass.id == models.TakeClass.subject_class_id)
+        .join(models.Subject, models.Subject.id == models.SubjectClass.id)
+        .join(models.Semester, models.Semester.id == models.SubjectClass.semester_id)
+        .join(models.CourseClass, models.CourseClass.id == models.Student.course_class_id)
+        .join(models.Major, models.Major.id == models.CourseClass.major_id)
+        .filter(
+            models.Student.id == student_id if student_id is not None else models.Student.id.isnot(None),
+            models.Semester.id == semester_id if semester_id is not None else models.Semester.id.isnot(None),
+            models.Student.name == student_name if student_name is not None else models.Student.name.isnot(None),
+            models.Student.date_of_birth == date_of_birth if date_of_birth is not None else models.Student.date_of_birth.isnot(None),
+           # models.Student.course_class_code == course_class_code if course_class_code is not None else models.Student.course_class_code.isnot(None),
+            func.concat("K",models.CourseClass.gen, "-", models.Major.code).label('course_class')== course_class_code if course_class_code is not None 
+            else models.Major.isnot(None),
+
+            models.SubjectClass.id == subject_class_id if subject_class_id is not None else models.SubjectClass.id.isnot(None),
+            models.Subject.subject_code == subject_code if subject_code is not None else models.Subject.subject_code.isnot(None),
+            models.Subject.subject_name == subject_name if subject_name is not None else models.Subject.subject_name.isnot(None),
+            models.Subject.credit == credit if credit is not None else models.Subject.credit.isnot(None)
+        ).order_by(
+            asc(models.Student.id),
+            asc(models.Student.name), 
+            asc(models.Student.date_of_birth),
+            asc('course_class_name'),
+            asc(models.SubjectClass.id),
+            asc(models.Subject.subject_code), 
+            asc(models.Subject.subject_name),
+            asc(models.Subject.credit) 
+        ).limit(limit_number)
+    )
+    result = query.all()
+    return result
+
--- app/main.py
+++ app/main.py
@@ -16,5 +16,18 @@ db=SessionLocal()
         
 @app.get("/subject/{subject_id}")
 def get_subject(subject_id: int):
-    return crud.get_subject(db, subject_id=subject_id)
+    return crud.get_subject(db, subject_id)
 
+#get infomarion of portal from student_id
+@app.get("/qldt")
+def qldt(student_id: int | None = None ,
+         student_name: str | None = None, date_of_birth: datetime | None = None,
+         course_class_code: str | None = None, subject_class_id: int | None = None,
+         subject_code: str | None = None, subject_name: str | None = None,
+         credit: int | None = None, limit_number: int | None = None, semester_id: int | None = None):
+    return crud.get_qldt(db, student_id, student_name,
+                         date_of_birth, course_class_code, subject_class_id,
+                         subject_code, subject_name, credit, limit_number, semester_id)
+# @app.get("/qldt2")
+# def qldt2(student_id: int | None = None , semester_id: int | None = None):
+#     return crud.get_qldt2(db, student_id, semester_id)
