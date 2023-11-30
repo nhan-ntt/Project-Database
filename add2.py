80d220df964d58e25d5089a4039a0e1c05ecdd08
nhanteefu <22028114@vnu.edu.vn>
2023-11-30 12:06:48 +0700

    update using env

--- add2.py
+++ add2.py
@@ -1,14 +1,28 @@
+import os
+import urllib.parse
+
+from dotenv import load_dotenv
 import pandas as pd
 import mysql.connector
+# Load environment variables from .env file
+load_dotenv()
 
-# MySQL Connection Parameters
+# Get database credentials from environment variables
+
+db_user = os.getenv("DB_USER")
+db_password = urllib.parse.quote_plus(os.getenv("DB_PASSWORD"))
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+decoded_password = urllib.parse.unquote_plus(db_password)
+
+
 mysql_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'Thanhnhan1911@',
-    'database': 'projectdb_quanlidaotao',
+    'host': db_host,
+    'user': db_user,
+    'password': decoded_password,
+    'database': db_name,
 }
-
 dfTake = pd.read_csv('app/models/takeclass.csv')
 dfTake.replace('', pd.NA, inplace=True)
 
